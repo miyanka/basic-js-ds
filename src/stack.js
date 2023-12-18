e19a3585ba972daa24e2e88c00608e755f01944b
Yana <bojcenko.jana@gmail.com>
2023-12-18 20:37:43 +0100

    feat: implement Stack

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  innerCollection = [];
+
+  push(element) {
+    this.innerCollection.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.innerCollection.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.innerCollection[this.innerCollection.length - 1];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
